0d3bff761130da76f44f81ff3f5f93264384978c
Muaz Yhaya <muazyhaya@outlook.com>
2021-07-23 07:16:02 -0700

    commit tree edcc11da06bb727773574a8856fe7c801d837775

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function superbowlWin(record) {
+    for (var i = 0; i < record.length; i++) {
+     if (record[i].result === "W") {
+         return record[i].year;
+     }
+    }
+}
+
+record.find(superbowlWin);
+     
 
